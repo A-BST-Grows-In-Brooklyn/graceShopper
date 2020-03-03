786cbcfb1dde0bc284c83664ed428940c30c4a8e
Nesara Kishor <nesara.zui@gmail.com>
2020-03-03 16:59:44 -0500

    allow-admin-to-create-slime-on-backend

--- server/api/slimes.js
+++ server/api/slimes.js
@@ -2,6 +2,23 @@ const router = require('express').Router()
 const {Slime} = require('../db/models')
 module.exports = router
 
+router.post('/:userId', async (req, res, next) => {
+  try {
+    if (req.user.admin) {
+      const newSlime = await Slime.create(req.body)
+      if (newSlime) {
+        res.status(201).json(newSlime)
+      } else {
+        res.status(404).send('Could Not Create Slime')
+      }
+    } else {
+      res.status(401).send('Not An Admin!')
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
 router.get('/:id', async (req, res, next) => {
   try {
     const slime = await Slime.findByPk(req.params.id)
