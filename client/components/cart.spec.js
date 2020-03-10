8b6678cda9e18dba111c67cdf5e6a3decb110cf3
Cass Spain <cea.spain@gmail.com>
2020-03-10 12:35:14 -0400

    guest cart db hooks done!

--- client/components/cart.spec.js
+++ client/components/cart.spec.js
@@ -2,7 +2,7 @@ import {expect} from 'chai'
 import React from 'react'
 import enzyme, {shallow} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import {Cart} from './cart'
+// import {Cart} from './cart'
 
 const adapter = new Adapter()
 enzyme.configure({adapter})
